618204b3239db9e4c81480c0860755634d58a006
Kovács Márk <kovacsm2@kkszki.hu>
2025-12-17 07:41:28 +0100

    Add project files.

---
+++ bevasarrlolista/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace bevasarrlolista
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
